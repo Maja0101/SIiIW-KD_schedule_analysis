5f6d8f02e5d832887386d19d64cf4b8ad21a9d23
Maja0101 <maja@kurczyna.com>
2026-03-15 15:09:32 +0100

    feat: a star

--- algorithms.py
+++ algorithms.py
@@ -6,6 +6,7 @@ from geopy.distance import geodesic
 
 TRANSFER_TIME = timedelta(minutes=5)
 AVG_TRAVEL_TIME = 86.08
+TRANSFER_PENALTY = timedelta(hours=12)
 
 class PriorityQueue:
     def __init__(self):
@@ -29,7 +30,7 @@ def runs_on_this_day(date, edge):
             return False
     return edge.weekdays[date.weekday()]
 
-def cost_by_time(current_datetime, current_route, edge):
+def calc_cost(current_datetime, current_route, edge):
     # print("cost by time")
     if current_datetime > edge.start_date and current_datetime < edge.end_date:
         # print("between dates")
@@ -86,30 +87,28 @@ def dijkstra_algorithm(graph, starting_point, starting_datetime):
 
         d, u = pq.pop()
 
-        if d > cost[u]['time']:
-            continue
-
         j = 0
         for v, e in graph.adj[u]:
             print("j = ", j)
             print("u: ", u, " v: ", v, " e: ", f"{e.departure_time}-{e.arrival_time} ({e.route_name})")
             
-            cost_u_v, transfer = cost_by_time(
+            time_u_v, transfer_u_v = calc_cost(
                 previous_route_part[u]['arrival_time'], 
                 previous_route_part[u]['route'], 
                 e)
             
-            print("cost: ", cost_u_v)
+            print("time cost: ", time_u_v)
+            print("transfer cost: ", time_u_v)
 
-            if cost_u_v < timedelta.max and (cost[v]['time'] > cost[u]['time'] + cost_u_v or (cost[v]['time'] == cost[u]['time'] + cost_u_v) and previous_route_part[u]['arrival_time'] + cost_u_v < previous_route_part[v]['arrival_time']):
-                cost[v]['time'] = cost[u]['time'] + cost_u_v
-                cost[v]['transfers'] = cost[u]['transfers'] + transfer
+            if time_u_v < timedelta.max and (cost[v]['time'] > cost[u]['time'] + time_u_v or (cost[v]['time'] == cost[u]['time'] + time_u_v and previous_route_part[u]['arrival_time'] + time_u_v < previous_route_part[v]['arrival_time'])):
+                cost[v]['time'] = cost[u]['time'] + time_u_v
+                cost[v]['transfers'] = cost[u]['transfers'] + transfer_u_v
                 previous_route_part[v]['stop'] = u
 
                 if u == starting_point:
                     previous_route_part[v]['arrival_time'] = datetime(previous_route_part[u]['arrival_time'].year, previous_route_part[u]['arrival_time'].month, previous_route_part[u]['arrival_time'].day) + e.arrival_time
                 else:
-                    previous_route_part[v]['arrival_time'] = previous_route_part[u]['arrival_time'] + cost_u_v
+                    previous_route_part[v]['arrival_time'] = previous_route_part[u]['arrival_time'] + time_u_v
                 previous_route_part[v]['departure_time'] = previous_route_part[v]['arrival_time'] - (e.arrival_time - e.departure_time)
                 previous_route_part[v]['route'] = e.route_name
                 pq.push(v, cost[v]['time'])
@@ -124,7 +123,90 @@ def dijkstra_algorithm(graph, starting_point, starting_datetime):
     return cost, previous_route_part
 
 def heuristics(first_stop, second_stop):
+    print("heuristics ", timedelta(seconds=int(geodesic(first_stop.coordinates, second_stop.coordinates).km / AVG_TRAVEL_TIME * 3600)) )
     return timedelta(seconds=int(geodesic(first_stop.coordinates, second_stop.coordinates).km / AVG_TRAVEL_TIME * 3600)) 
+
+def a_star_algorithm(graph, starting_point, ending_point, starting_datetime, optimize_by_time=True):
+    open_v = PriorityQueue()
+    closed_v = set()
+
+    cost = {key: {'time': timedelta.max, 'transfers': maxsize} for key in graph.nodes.keys()}
+
+    previous_route_part = {key: {'stop': -1, 'route': None, 'departure_time': None, 'arrival_time': None}  for key in graph.nodes.keys()} 
+
+    cost[starting_point]['time'] = timedelta(0)
+    cost[starting_point]['transfers'] = 0
+
+    previous_route_part[starting_point]['arrival_time'] = starting_datetime
+
+    open_v.push(starting_point, timedelta(0))
+
+    i = 0
+    while not open_v.is_empty():
+        print("i = ", i)
+
+        d, u = open_v.pop()
+
+        print("got with f ", d)
+
+        if u == ending_point:
+            return cost, previous_route_part
+
+        closed_v.add(u)
+
+        j = 0
+        for v, e in graph.adj[u]:
+            print("j = ", j)
+            print("u: ", u, " v: ", v, " e: ", f"{e.departure_time}-{e.arrival_time} ({e.route_name})")
+
+            print("cost: ", cost)
+            print("previous_route_part: ", previous_route_part)
+
+            if v in closed_v:
+                continue
+
+            time_u_v, transfer_u_v = calc_cost(
+                previous_route_part[u]['arrival_time'], 
+                previous_route_part[u]['route'], 
+                e)
+            
+            print("time cost: ", time_u_v)
+            print("transfer cost: ", transfer_u_v)
+
+            if (optimize_by_time and (
+                time_u_v < timedelta.max and (
+                    cost[v]['time'] > cost[u]['time'] + time_u_v or (
+                        cost[v]['time'] == cost[u]['time'] + time_u_v and previous_route_part[u]['arrival_time'] + time_u_v < previous_route_part[v]['arrival_time'])))) or (
+                not optimize_by_time and (
+                    transfer_u_v < maxsize and (
+                        cost[v]['transfers'] > cost[u]['transfers'] + transfer_u_v or (
+                            cost[v]['transfers'] == cost[u]['transfers'] + transfer_u_v and previous_route_part[u]['arrival_time'] + time_u_v < previous_route_part[v]['arrival_time'])))
+                ):
+                    
+                cost[v]['time'] = cost[u]['time'] + time_u_v
+                cost[v]['transfers'] = cost[u]['transfers'] + transfer_u_v
+                previous_route_part[v]['stop'] = u
+
+                if u == starting_point:
+                    previous_route_part[v]['arrival_time'] = datetime(previous_route_part[u]['arrival_time'].year, previous_route_part[u]['arrival_time'].month, previous_route_part[u]['arrival_time'].day) + e.arrival_time
+                else:
+                    previous_route_part[v]['arrival_time'] = previous_route_part[u]['arrival_time'] + time_u_v
+                previous_route_part[v]['departure_time'] = previous_route_part[v]['arrival_time'] - (e.arrival_time - e.departure_time)
+                previous_route_part[v]['route'] = e.route_name
+
+                f = cost[v]['time'] + heuristics(graph.nodes[v], graph.nodes[ending_point])
+                if not optimize_by_time:
+                    f += cost[v]['transfers'] * TRANSFER_PENALTY
+                open_v.push(v, f)
+
+                print("adding with f ", f)
+
+            j += 1
+
+        i += 1
+
+    return cost, previous_route_part
+
 
 def user_route(starting_point, ending_point, algorithm_results):
     cost, route_part = algorithm_results
@@ -161,32 +243,57 @@ def user_route(starting_point, ending_point, algorithm_results):
 
     return cost[ending_point], route
 
+def display_user_route(graph, user_route):
+    cost, route = user_route
+    
+    for r in route:
+        via = ', '.join(graph.nodes[i].name for i in  r['stops'][1:-1] if i in graph.nodes)
+        print(f"Train: {r['route_name']} Departure from {graph.nodes.get(r['stops'][0]).name} at {r['start_time'].strftime('%Y-%m-%d %H:%M')} {f'via {via} ' if via else ''}arrival at {graph.nodes.get(r['stops'][-1]).name} at {r['end_time'].strftime('%Y-%m-%d %H:%M')}")
+
+    print(f"Travel time: {cost['time']} Number of transfers: {cost['transfers']}")
+
 
 if __name__ == '__main__':
     graph = Graph()
 
-    graph.add_node(0, Node('A', None, None))
-    graph.add_node(1, Node('B', None, None))
-    graph.add_node(2, Node('C', None, None))
-    graph.add_node(3, Node('D', None, None))
-    graph.add_node(4, Node('E', None, None))
-    graph.add_node(5, Node('F', None, None))
+    graph.add_node(0, Node('A', 50, 15))
+    graph.add_node(1, Node('B', 50, 16))
+    graph.add_node(2, Node('C', 51, 16))
+    graph.add_node(3, Node('D', 51, 17))
+    graph.add_node(4, Node('E', 52, 15))
+    graph.add_node(5, Node('F', 52, 16))
+    graph.add_node(6, Node('G', 51, 18))
+
+    # graph.add_edge(0, 1, Edge(timedelta(0, hours=10, minutes=10), timedelta(0, hours=10, minutes=13), 'a', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
+    # graph.add_edge(0, 4, Edge(timedelta(0, hours=10, minutes=16), timedelta(0, hours=10, minutes=19), 'b', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, False, True], {date(2026, 3, 14): 1}))
+    # graph.add_edge(1, 2, Edge(timedelta(0, hours=10, minutes=13), timedelta(0, hours=10, minutes=14), 'a', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
+    # graph.add_edge(2, 3, Edge(timedelta(0, hours=10, minutes=10), timedelta(0, hours=10, minutes=11), 'd', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
+    # graph.add_edge(2, 5, Edge(timedelta(0, hours=10, minutes=14), timedelta(0, hours=10, minutes=15), 'a', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
+    # graph.add_edge(3, 1, Edge(timedelta(0, hours=10, minutes=35), timedelta(0, hours=10, minutes=36), 'c', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
+    # graph.add_edge(4, 5, Edge(timedelta(0, hours=10, minutes=20), timedelta(0, hours=10, minutes=21), 'b', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
+    # graph.add_edge(5, 0, Edge(timedelta(0, hours=10, minutes=15), timedelta(0, hours=10, minutes=21), 'a', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
+    # graph.add_edge(5, 3, Edge(timedelta(0, hours=10, minutes=30), timedelta(0, hours=10, minutes=35), 'c', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
+    # graph.add_edge(3, 6, Edge(timedelta(0, hours=10, minutes=46), timedelta(0, hours=10, minutes=50), 'd', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
 
     graph.add_edge(0, 1, Edge(timedelta(0, hours=10, minutes=10), timedelta(0, hours=10, minutes=13), 'a', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
-    graph.add_edge(0, 4, Edge(timedelta(0, hours=10, minutes=16), timedelta(0, hours=10, minutes=19), 'b', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, False, True], {date(2026, 3, 14): 1}))
-    graph.add_edge(1, 2, Edge(timedelta(0, hours=10, minutes=13), timedelta(0, hours=10, minutes=14), 'a', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
+    graph.add_edge(0, 4, Edge(timedelta(0, hours=10, minutes=10), timedelta(0, hours=10, minutes=31), 'b', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, False, True], {date(2026, 3, 14): 1}))
+    graph.add_edge(1, 2, Edge(timedelta(0, hours=10, minutes=18), timedelta(0, hours=10, minutes=20), 'e', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
     graph.add_edge(2, 3, Edge(timedelta(0, hours=10, minutes=10), timedelta(0, hours=10, minutes=11), 'd', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
-    graph.add_edge(2, 5, Edge(timedelta(0, hours=10, minutes=14), timedelta(0, hours=10, minutes=15), 'a', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
+    graph.add_edge(2, 5, Edge(timedelta(0, hours=10, minutes=26), timedelta(0, hours=10, minutes=36), 'f', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
     graph.add_edge(3, 1, Edge(timedelta(0, hours=10, minutes=35), timedelta(0, hours=10, minutes=36), 'c', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
-    graph.add_edge(4, 5, Edge(timedelta(0, hours=10, minutes=20), timedelta(0, hours=10, minutes=21), 'b', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
-    graph.add_edge(5, 1, Edge(timedelta(0, hours=10, minutes=15), timedelta(0, hours=10, minutes=21), 'a', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
-    graph.add_edge(5, 3, Edge(timedelta(0, hours=10, minutes=30), timedelta(0, hours=10, minutes=35), 'c', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
+    graph.add_edge(4, 5, Edge(timedelta(0, hours=10, minutes=32), timedelta(0, hours=10, minutes=36), 'b', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
+    graph.add_edge(5, 0, Edge(timedelta(0, hours=10, minutes=15), timedelta(0, hours=10, minutes=21), 'a', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
+    graph.add_edge(5, 3, Edge(timedelta(0, hours=10, minutes=41), timedelta(0, hours=10, minutes=45), 'c', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
+    graph.add_edge(3, 6, Edge(timedelta(0, hours=10, minutes=46), timedelta(0, hours=10, minutes=50), 'd', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
+    graph.add_edge(5, 3, Edge(timedelta(0, hours=10, minutes=36), timedelta(0, hours=10, minutes=55), 'b', datetime(2026, 3, 3), datetime(2026, 12, 12), [True, True, True, True, True, True, True], {}))
 
-    cost, previous_route_part = dijkstra_algorithm(graph, 0, datetime(2026, 3, 14, 10, 10, 00))
+    # cost, previous_route_part = dijkstra_algorithm(graph, 0, datetime(2026, 3, 14, 10, 10, 00))
+    cost, previous_route_part = a_star_algorithm(graph, 0, 3, datetime(2026, 3, 14, 10, 10, 00), False)
 
     print("end")
     print("cost: ", cost)
     print("previous_route_part: ", previous_route_part)
 
-    user_route  = user_route(0, 5, (cost, previous_route_part))
+    user_route  = user_route(0, 3, (cost, previous_route_part))
 
+    display_user_route(graph, user_route)
