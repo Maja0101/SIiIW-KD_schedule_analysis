255eed8a7a395f013211cb2db522257616e66502
Maja0101 <maja@kurczyna.com>
2026-03-20 09:29:32 +0100

    fix: assign exceptions

--- load_data.py
+++ load_data.py
@@ -50,7 +50,7 @@ def create_graph(
     calendar_dates['date'] = pd.to_datetime(calendar_dates['date'], format="%Y%m%d").dt.date
 
     # data nessesary for one travel section
-    previus_stop_id = None
+    previous_stop_id = None
     next_stop_id = None
     route_name = ""
     start_date = None
@@ -63,8 +63,8 @@ def create_graph(
     # iterating through stop_times, creating edges and appending them do adj list in graph
     for row in stop_times.itertuples(index=False):
         if row.stop_sequence == 0:
-            previus_stop_id = stops.loc[row.stop_id]
-            previus_stop_id = int(previus_stop_id['parent_station']) if pd.notna(previus_stop_id['parent_station']) else previus_stop_id.name
+            previous_stop_id = stops.loc[row.stop_id]
+            previous_stop_id = int(previous_stop_id['parent_station']) if pd.notna(previous_stop_id['parent_station']) else previous_stop_id.name
 
             departure_time = row.departure_time
 
@@ -72,16 +72,20 @@ def create_graph(
             route_name = route['route_short_name'] if not pd.isna(route['route_short_name']) else route['route_long_name']
 
             service_id = trips.loc[row.trip_id]['service_id']
+
             operating_time = calendar.loc[service_id]
             start_date = operating_time['start_date']
             end_date = operating_time['end_date']
+
             weekdays_names = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
             weekdays = [True if operating_time[day] == 1 else False for day in weekdays_names]
             
             route_exceptions = calendar_dates.loc[calendar_dates['service_id'] == service_id]
+            exceptions = {}
             if not route_exceptions.empty:
                 for row in route_exceptions.itertuples():
                     exceptions[row.date] = row.exception_type
+
         else:
             next_stop_id = stops.loc[row.stop_id]
             next_stop_id = int(next_stop_id['parent_station']) if pd.notna(next_stop_id['parent_station']) else next_stop_id.name
@@ -90,9 +94,9 @@ def create_graph(
 
             edge = Edge(departure_time, arrival_time, route_name, start_date, end_date, weekdays, exceptions)
 
-            graph.add_edge(previus_stop_id, next_stop_id, edge)
+            graph.add_edge(previous_stop_id, next_stop_id, edge)
 
-            previus_stop_id = next_stop_id
+            previous_stop_id = next_stop_id
             departure_time = row.departure_time
 
     return graph
